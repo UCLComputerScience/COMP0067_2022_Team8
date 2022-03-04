530a88ab86b7ee336e043986dd7cd93e40e9d759
Ohhun Kwon <ohhunkwon@hotmail.com>
2022-03-04 11:19:57 +0000

    fix: mobile responsive gallery

--- client/src/components/VideoCards.js
+++ client/src/components/VideoCards.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './VideoCards.css';
-import '../App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Button } from './Button';
@@ -13,22 +12,24 @@ function VideoCards() {
     axios.get("http://localhost:3001/videos").then((response) => {
       setListOfVideos(response.data);
     });
-  }, [])
+  }, []);
 
   return (
     <div className='cards background'>
       <h1 className='gallery-title'>Gallery</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
-          {listOfVideos.map((value, key) => {
-            return (
-              <div className="cards__item video">
-                <div className="title">{value.title}</div>
-                <iframe className="url" width="396" height="300" src={value.url}></iframe>
-                <div className="description">{value.description}</div>
-              </div>
-            );
-          })}
+          <ul className='grid-layout'>
+            {listOfVideos.map((value, idx) => {
+              return (
+                <li className="video">
+                    <div className="title">{value.title}</div>
+                    <iframe className="url" width="396" height="300" src={value.url}></iframe>
+                    <div className="description">{value.description}</div>
+                </li>
+              )
+            })}
+          </ul>
         </div>
       </div>
       <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>See more here</Button>
