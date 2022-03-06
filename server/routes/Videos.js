ac330ab9cd0bb901260afd666abaccd26667b97a
Ohhun Kwon <ohhunkwon@hotmail.com>
2022-03-06 01:58:45 +0000

    feat: csv update & server restart updates db & front-end

--- server/routes/Videos.js
+++ server/routes/Videos.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const { Videos } = require("../models");
+const fs = require('fs');
+const csv = require('csv');
+
+let input = fs.createReadStream('./csv/videos.csv');
+let parser = csv.parse({
+    delimiter: ',',
+    columns: true
+})
+
+var transform = csv.transform(function(row) {
+    var resultObj = {
+        title: row['title'],
+        description: row['description'],
+        url: row['url']
+    }
+    Videos.create(resultObj)
+        .then(function() {
+            console.log('Record created');
+        })
+        .catch(function(err) {
+            console.log('Error encountered: ' + err);
+        })
+})
+
+input.pipe(parser).pipe(transform)
 
 router.get("/", async (req, res) => {
     const listOfVideos = await Videos.findAll();
