6d10601eeaaa579cd09465edbf4075253ad00662
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2022-03-13 22:35:53 +0000

    added basic vids section
    just added ohhuns code to home page segment, needs parameters to show recently added, new route?

--- client/src/components/RecentVideos.js
+++ client/src/components/RecentVideos.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './RecentVideos.css';
+import axios from 'axios';
+
 
 function RecentVideos() {
+    const [listOfVideos, setListOfVideos] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/videos").then((response) => {
+            setListOfVideos(response.data);
+        });
+    }, []);
+
     return (
-        <div className='cards background'>
+        <div className='cards'>
             <h1>Recent Videos</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='grid-layout'>
-
+                        {listOfVideos.map((value, idx) => {
+                            return (
+                                <li className="video">
+                                    <div className="title">{value.title}</div>
+                                    <iframe className="url" width="396" height="300" src={value.url}></iframe>
+                                    <div className="description">{value.description}</div>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
         </div>
     )
 }
+
+export default RecentVideos;
