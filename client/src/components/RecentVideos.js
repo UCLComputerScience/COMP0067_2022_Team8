b93ec58233238d4cab5a681042d5a58e6ff2f4aa
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2022-03-13 18:12:31 +0000

    recent videos section
    work in progress

---
+++ client/src/components/RecentVideos.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './RecentVideos.css';
+
+function RecentVideos() {
+    return (
+        <div className='cards background'>
+            <h1>Recent Videos</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='grid-layout'>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
