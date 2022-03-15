c710c44c04cef4b8f8bdc089ddc2dfc169c1de74
Ohhun Kwon <ohhunkwon@hotmail.com>
2022-03-15 08:40:15 +0000

    fix: csv functionality added back in

--- server/index.js
+++ server/index.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { Videos } = require("./models");
 app.use(express.json());
 app.use(cors());
 
 const db = require('./models');
 
-// db.Videos.destroy({
-//     where: {},
-//     truncate: true
-// })
+db.Videos.destroy({
+    where: {},
+    truncate: true
+})
 
 // Routers
 const videoRouter = require('./routes/Videos');
 app.use("/videos", videoRouter);
 
-// db.sequelize.sync().then(() => {
-//     app.listen(process.env.PORT || 3001, () => {
-//         console.log("Server running on port 3001...");
-//     });
-// });
-
-app.listen(process.env.PORT || 3001, () => {
-    console.log("Server running on port 3001...");
+db.sequelize.sync().then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+        console.log("Server running on port 3001...");
+    });
 });
