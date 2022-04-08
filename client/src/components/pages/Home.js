f5f6005cc618ddd21fde3f7e79e61c2d8794b0f2
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2022-04-08 19:25:54 +0100

    testing blog cards

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,13 +3,14 @@ import '../../App.css';
 import Cards from '../Cards';
 import RecentVideos from '../RecentVideos'
 import HeroSection from '../HeroSection';
+import Showblogs from '../showblogs';
 
 function Home() {
   return (
     <>
       <HeroSection />
       <RecentVideos />
-      <Cards />
+      <Showblogs />
     </>
   );
 }
