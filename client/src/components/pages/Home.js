ef87d2197d2837a69de0fb726745e77f4e5d08dc
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2022-04-12 13:43:49 +0100

    Revert "testing blog cards"
    This reverts commit f5f6005cc618ddd21fde3f7e79e61c2d8794b0f2.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,14 +3,13 @@ import '../../App.css';
 import Cards from '../Cards';
 import RecentVideos from '../RecentVideos'
 import HeroSection from '../HeroSection';
-import Showblogs from '../showblogs';
 
 function Home() {
   return (
     <>
       <HeroSection />
       <RecentVideos />
-      <Showblogs />
+      <Cards />
     </>
   );
 }
