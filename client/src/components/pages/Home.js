dd3e67b0f749836165bd5fdb7bf95600ea20244d
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2022-04-13 20:40:41 +0100

    Revert "fetching medium data"
    This reverts commit 8628c58d86316cf3481a6eb5c3ea4a5b9ba87bdf.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import MedBlog from '../MedBlogs';
+import Cards from '../Cards';
 import RecentVideos from '../RecentVideos'
 import HeroSection from '../HeroSection';
 
@@ -9,7 +9,7 @@ function Home() {
     <>
       <HeroSection />
       <RecentVideos />
-      <MedBlog />
+      <Cards />
     </>
   );
 }
