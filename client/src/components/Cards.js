bd4ee581f4b42d089575d71bc70fa6d7cae77664
Ohhun Kwon <ohhunkwon@hotmail.com>
2022-04-19 14:40:22 +0100

    fix: dummy blog cards added

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -39,6 +39,20 @@ function Cards() {
               );}
             )}
             <CardItem
+              src='https://miro.medium.com/max/1048/1*nqQBGAxWHR-eCoK3751K1w.png'
+              text='SIN France organises first UK-France Educational Hackathon on AI for the Common Good at Paris British Embassy'
+        
+              label='Medium blog'
+              path='https://medium.com/@infoixnforgood/sin-france-organises-first-uk-france-educational-hackathon-on-ai-for-the-common-good-at-paris-3240cd710975'
+            />
+            <CardItem
+              src='https://miro.medium.com/max/1400/0*cFaSDNpDLLGeDSbW.jpg'
+              text='Tackling Challenges in Food poverty to build a #HungerFreeFuture'
+        
+              label='Medium blog'
+              path='https://medium.com/@infoixnforgood/tackling-challenges-in-food-poverty-to-build-a-hungerfreefuture-f48e0c1184a6'
+            />
+            <CardItem
               src='https://i.stack.imgur.com/twIm6.png'
               text='See all case studies in our Medium blog page'
         
