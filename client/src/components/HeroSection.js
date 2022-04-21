49b29e4a245e9912cb5e9699691afa82f26783d8
Ohhun Kwon <ohhunkwon@hotmail.com>
2022-04-21 16:30:33 +0100

    fix: spelling

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom";
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <h1>Enpowering Charities with the Art of Possible.</h1>
+      <h1>Empowering Charities with the Art of Possible.</h1>
       <p>Industry Exchange Network For Good</p>
       <div className='hero-btns'>
         <Button
